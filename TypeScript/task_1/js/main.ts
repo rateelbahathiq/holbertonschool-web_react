c732e0082698f0e12870d42d4be785e23e697d12
RateelB <Bahathiqratee@gmail.com>
2026-04-18 09:56:12 +0300

    Task 1: complete Teacher, Directors, and printTeacher function

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -22,3 +22,16 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+// Interface for the function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Function implementation
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Test
+console.log(printTeacher("John", "Doe"));
