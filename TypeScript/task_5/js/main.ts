397b6a3bdc9b0c0afa8698608b88885b4065f75b
RateelB <Bahathiqratee@gmail.com>
2026-04-18 11:01:35 +0300

    Task 5: implement nominal typing with branded interfaces

---
+++ TypeScript/task_5/js/main.ts
@@ -0,0 +1,23 @@
+interface MajorCredits {
+  credits: number;
+  readonly __brand: 'MajorCredits';
+}
+
+interface MinorCredits {
+  credits: number;
+  readonly __brand: 'MinorCredits';
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MajorCredits',
+  };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MinorCredits',
+  };
+}
